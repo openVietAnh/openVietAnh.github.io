9a4c1ce635d3d99bdbeae31d89e8d1ae7e4cb58b
openVietAnh <vietanhtran.uet@gmail.com>
2025-01-04 21:51:54 +0700

    Initialize and Integrate with the Skeleton UI Toolkit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,22 @@
 <script lang="ts">
 	import '../app.css';
+	import { AppBar, AppShell } from '@skeletonlabs/skeleton';
 	let { children } = $props();
 </script>
 
-{@render children()}
+<AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
+	<svelte:fragment slot="sidebarLeft">
+		<!-- Insert the list: -->
+		<nav class="list-nav">
+			<ul>
+				<li><a href="/">Home</a></li>
+				<!-- <li><a href="/about">About</a></li> -->
+			</ul>
+		</nav>
+		<!-- --- -->
+	</svelte:fragment>
+	{@render children()}
+	<svelte:fragment slot="header">
+		<AppBar>Skeleton</AppBar>
+	</svelte:fragment>
+</AppShell>
